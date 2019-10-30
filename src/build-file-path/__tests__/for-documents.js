e87305226cd4dddefb21447874c9c021d9bcd2e4
Pavel Bucka <ppenge@gmail.com>
2019-10-30 16:53:37 +0000

    Documents can have other name than index.md

--- src/build-file-path/__tests__/for-documents.js
+++ src/build-file-path/__tests__/for-documents.js
@@ -9,9 +9,9 @@
 const { buildFilePath } = require('../../../');
 
 test('buildPath', () => {
-  /*
-    1) mdPath is in "documents" root
-  */
+
+// 1. "index.md" is changed to "index.html"
+
   expect(buildFilePath(
     '~/environment/blog/',
     '~/environment/blog/documents/index.md'
@@ -19,9 +19,6 @@ test('buildPath', () => {
     '~/environment/blog/_build/index.html'
   );
 
-  /*
-    2) mdPath is in "documents" nested folder
-  */
   expect(buildFilePath(
     '~/environment/blog/',
     '~/environment/blog/documents/contact/index.md'
@@ -29,9 +26,6 @@ test('buildPath', () => {
     '~/environment/blog/_build/contact/index.html'
   );
 
-  /*
-    3) mdPath is in "documents" nested documents folder
-  */
   expect(buildFilePath(
     '~/environment/blog/',
     '~/environment/blog/documents/travel/documents/index.md'
@@ -39,13 +33,51 @@ test('buildPath', () => {
     '~/environment/blog/_build/travel/documents/index.html'
   );
 
-  /*
-    4) mdPath is in "documents" nested .md folder
-  */
   expect(buildFilePath(
     '~/environment/blog/',
     '~/environment/blog/documents/extentions/.md/index.md'
   )).toBe(
     '~/environment/blog/_build/extentions/.md/index.html'
+  );
+
+
+// 2. "404.md" (only in root) is changed to "404.html"
+
+  expect(buildFilePath(
+    '~/environment/blog/',
+    '~/environment/blog/documents/404.md'
+  )).toBe(
+    '~/environment/blog/_build/404.html'
+  );
+
+
+// 3. "<other>.md" is changed to "/other/index.html"
+
+  expect(buildFilePath(
+    '~/environment/blog/',
+    '~/environment/blog/documents/changelog.md'
+  )).toBe(
+    '~/environment/blog/_build/changelog/index.html'
+  );
+
+  expect(buildFilePath(
+    '~/environment/blog/',
+    '~/environment/blog/documents/about.md'
+  )).toBe(
+    '~/environment/blog/_build/about/index.html'
+  );
+
+  expect(buildFilePath(
+    '~/environment/blog/',
+    '~/environment/blog/documents/authors/pavel.md'
+  )).toBe(
+    '~/environment/blog/_build/authors/pavel/index.html'
+  );
+
+  expect(buildFilePath(
+    '~/environment/blog/',
+    '~/environment/blog/documents/api/cars/GET.md'
+  )).toBe(
+    '~/environment/blog/_build/api/cars/GET/index.html'
   );
 });
--- src/build-file-path/index.js
+++ src/build-file-path/index.js
@@ -9,11 +9,16 @@ function buildFilePath(cwd, mdPath) {
   }
 
   const relativeSrc = path.relative(cwd, mdPath);
-  const relativeDest = relativeSrc
-    .replace(/^documents/,'_build')
-    .replace(/\.md$/, '.html');
+  const relativeDest = relativeSrc.replace(/^documents/,'_build');
 
-  return path.join(cwd, relativeDest);
+  const dirname = path.dirname(relativeDest);
+  const basename = path.basename(relativeDest, '.md');
+
+  const filename = ['index', '404'].includes(basename)
+    ? basename + '.html'
+    : path.join(basename, 'index.html');
+
+  return path.join(cwd, dirname, filename);
 }
 
 module.exports = buildFilePath;
